e6ca12aa3c7488b79bedf5ad679a608188827b6d
Damian Logghe <damian@timersys.com>
2017-06-08 22:07:51 -0300

    new files includes

--- src/functions_include.php
+++ src/functions_include.php
@@ -4,4 +4,6 @@ if ( !function_exists('GeotFunctions\toArray') ) {
 	require __DIR__ . '/functions.php';
 	require __DIR__ . '/filters.php';
 	require __DIR__ . '/global-functions.php';
+	require __DIR__ . '/database.php';
+	require __DIR__ . '/plugins.php';
 }

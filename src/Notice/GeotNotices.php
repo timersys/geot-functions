4272a432a1125bc703b4b848bee9085e4d296295
Damian Logghe <damian@timersys.com>
2018-06-08 09:58:30 -0300

    fix notice to show only to wpengine users

--- src/Notice/GeotNotices.php
+++ src/Notice/GeotNotices.php
@@ -14,15 +14,16 @@ class GeotNotices {
 		if( isset( $_GET['geot_notice'])){
 			update_option('geot_'.esc_attr($_GET['geot_notice']), true);
 		}
-        if( ! get_option('spu_wpengine_dismiss') )
+
+		if(  getenv( 'HTTP_GEOIP_COUNTRY_CODE' ) !== false && ! get_option('geot_wpengine_dismiss') )
             add_action( 'admin_notices', array( self::class, 'wpengine') );
 	}
 
 	public static function wpengine(){
 		?><div class="notice-info error">
-		<h3><i class=" dashicons-before dashicons-admin-site"></i>GeotargetingWP WPEngine</h3>
+		<h3><i class=" dashicons-before dashicons-admin-site"></i> GeotargetingWP WPEngine</h3>
 		<p>We detected that your have WPEngine Geolocation enabled on your hosting.</p>
-		<p>Please go to the <a href="<?php echo admin_url('admin.php?page=geot-settings');?>">settings page</a> and enable it for using it with GeotargetingWP plugins.</p>
+		<p>Please go to the <a href="<?php echo admin_url('admin.php?page=geot-settings');?>">settings page</a> and enable it for using it with the GeotargetingWP plugins.</p>
 		<p><a href="<?= admin_url('?geot_notice=wpengine_dismiss');?>" class="button-primary"><?php _e('Dismiss','geot');?></a></p>
 		</div><?php
 	}

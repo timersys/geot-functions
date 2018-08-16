8e17c90c199ab2025fbbccc3fbdbc6a9ec736936
DESKTOP-2P361D7\gonzalesc <alex@gonzalesc.org>
2018-08-16 17:25:32 -0300

    Custom Debug Data and Download in TXT

---
+++ src/Setting/partials/download-debug-data.php
@@ -0,0 +1,11 @@
+<?php
+if( isset($_POST['geot-debug-button']) &&
+	isset($_POST['geot-debug-content']) &&
+	isset($_POST['geot-debug-nonce'])
+) {
+	header("Content-type: text/plain");
+	header("Content-Disposition: attachment; filename=debug-data.txt");
+
+	echo $_POST['geot-debug-content'];
+}
+?>

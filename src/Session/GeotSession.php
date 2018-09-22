c3a488aa8045fd2735809ce0bba5c2731d7f53f9
Damian Logghe <damian@timersys.com>
2018-09-22 09:25:01 -0300

    undefined error and reformat code

--- src/Session/GeotSession.php
+++ src/Session/GeotSession.php
@@ -1,5 +1,7 @@
 <?php
+
 namespace GeotFunctions\Session;
+
 use EAMann\Sessionz\Handlers\EncryptionHandler;
 use EAMann\Sessionz\Handlers\MemoryHandler;
 use EAMann\Sessionz\Manager;
@@ -44,42 +46,42 @@ class GeotSession {
 	 */
 	public function __construct() {
 
-        if( ! $this->should_start_session() ) {
-           return;
-        }
+		if ( ! $this->should_start_session() ) {
+			return;
+		}
 
 		// Queue up the session stack
 		$wp_session_handler = Manager::initialize();
-		if (defined('WP_SESSION_USE_OPTIONS') && WP_SESSION_USE_OPTIONS) {
-		    $wp_session_handler->addHandler(new OptionsHandler());
+		if ( defined( 'WP_SESSION_USE_OPTIONS' ) && WP_SESSION_USE_OPTIONS ) {
+			$wp_session_handler->addHandler( new OptionsHandler() );
 		} else {
-		    $wp_session_handler->addHandler(new DatabaseHandler());
+			$wp_session_handler->addHandler( new DatabaseHandler() );
 		}
 
-		if (defined('WP_SESSION_ENC_KEY') && WP_SESSION_ENC_KEY) {
-		    $wp_session_handler->addHandler(new EncryptionHandler(WP_SESSION_ENC_KEY));
+		if ( defined( 'WP_SESSION_ENC_KEY' ) && WP_SESSION_ENC_KEY ) {
+			$wp_session_handler->addHandler( new EncryptionHandler( WP_SESSION_ENC_KEY ) );
 		}
 
-		$wp_session_handler->addHandler(new MemoryHandler());
+		$wp_session_handler->addHandler( new MemoryHandler() );
 
 		// Create the required table.
-		add_action('admin_init',      ['EAMann\WPSession\DatabaseHandler', 'create_table']);
-		add_action('wp_session_init', ['EAMann\WPSession\DatabaseHandler', 'create_table']);
-		add_action('wp_install',      ['EAMann\WPSession\DatabaseHandler', 'create_table']);
+		add_action( 'admin_init', [ 'EAMann\WPSession\DatabaseHandler', 'create_table' ] );
+		add_action( 'wp_session_init', [ 'EAMann\WPSession\DatabaseHandler', 'create_table' ] );
+		add_action( 'wp_install', [ 'EAMann\WPSession\DatabaseHandler', 'create_table' ] );
 
 		// Start up session management, if we're not in the CLI
-		if (!defined('WP_CLI') || false === WP_CLI) {
-		    add_action('plugins_loaded', [$this, 'wp_session_manager_start_session'], 10, 0);
+		if ( ! defined( 'WP_CLI' ) || false === WP_CLI ) {
+			add_action( 'plugins_loaded', [ $this, 'wp_session_manager_start_session' ], 10, 0 );
 		}
 	}
 
 	/**
 	 * If a session hasn't already been started by some external system, start one!
 	 */
-	function wp_session_manager_start_session()	{
-	    if (session_status() !== PHP_SESSION_ACTIVE) {
-	        session_start();
-	    }
+	function wp_session_manager_start_session() {
+		if ( session_status() !== PHP_SESSION_ACTIVE ) {
+			session_start();
+		}
 	}
 
 	/**
@@ -96,6 +98,7 @@ class GeotSession {
 		if ( is_null( self::$_instance ) ) {
 			self::$_instance = new self();
 		}
+
 		return self::$_instance;
 	}
 
@@ -120,13 +123,15 @@ class GeotSession {
 	 *
 	 * @access public
 	 * @since 1.5
+	 *
 	 * @param string $key Session key
+	 *
 	 * @return mixed Session variable
 	 */
 	public function get( $key ) {
-		$key    = sanitize_key( $key );
+		$key = sanitize_key( $key );
 
-		return json_decode($_SESSION[ $key ] ) ?: false;
+		return isset( $_SESSION[ $key ] ) ? json_decode( $_SESSION[ $key ] ) : false;
 	}
 
 	/**
@@ -136,6 +141,7 @@ class GeotSession {
 	 *
 	 * @param string $key Session key
 	 * @param int|string|array $value Session variable
+	 *
 	 * @return mixed Session variable
 	 */
 	public function set( $key, $value ) {
@@ -154,19 +160,20 @@ class GeotSession {
 	 */
 	public function should_start_session() {
 		$start_session = true;
-		if( ! empty( $_SERVER[ 'REQUEST_URI' ] ) ) {
+		if ( ! empty( $_SERVER['REQUEST_URI'] ) ) {
 			$blacklist = $this->get_blacklist();
-			$uri       = ltrim( $_SERVER[ 'REQUEST_URI' ], '/' );
+			$uri       = ltrim( $_SERVER['REQUEST_URI'], '/' );
 			$uri       = untrailingslashit( $uri );
-			if( in_array( $uri, $blacklist ) ) {
+			if ( in_array( $uri, $blacklist ) ) {
 				$start_session = false;
 			}
-			if( false !== strpos( $uri, 'feed=' ) ) {
+			if ( false !== strpos( $uri, 'feed=' ) ) {
 				$start_session = false;
 			}
 		}
-		if( isset($_GET['page']) && 'geot-debug-data' == $_GET['page'] )
+		if ( isset( $_GET['page'] ) && 'geot-debug-data' == $_GET['page'] ) {
 			$start_session = false;
+		}
 
 		return apply_filters( 'geot/sessions/start_session', $start_session );
 	}
@@ -190,11 +197,12 @@ class GeotSession {
 		) );
 		// Look to see if WordPress is in a sub folder or this is a network site that uses sub folders
 		$folder = str_replace( network_home_url(), '', get_site_url() );
-		if( ! empty( $folder ) ) {
-			foreach( $blacklist as $path ) {
+		if ( ! empty( $folder ) ) {
+			foreach ( $blacklist as $path ) {
 				$blacklist[] = $folder . '/' . $path;
 			}
 		}
+
 		return $blacklist;
 	}
 }
